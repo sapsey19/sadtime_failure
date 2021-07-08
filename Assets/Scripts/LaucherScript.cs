8eba014730c6474b2da91182639394c64ad6c54a
Sean Apsey <sapsey19@gmail.com>
2021-07-07 20:04:38 -0700

    uhhh level prototype

--- Assets/Scripts/LaucherScript.cs
+++ Assets/Scripts/LaucherScript.cs
@@ -7,9 +7,18 @@ public class LaucherScript : MonoBehaviour {
     public GameObject rocket;
     public Transform spawnPos;
 
+    public float delay = 1f;
+
+    float time = 0;
+
     void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            Instantiate(rocket, spawnPos.transform.position, transform.rotation);
+        time += Time.deltaTime;
+        Debug.Log(time);
+        if (time > delay) {
+            if (Input.GetMouseButtonDown(0)) {
+                Instantiate(rocket, spawnPos.transform.position, transform.rotation);
+                time = 0;
+            }
         }
     }
 }
