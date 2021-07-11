ef9d166462340067b52d90cf8e76e7c3610235f5
Sean Apsey <sapsey19@gmail.com>
2021-07-11 13:54:52 -0700

    better rockets

--- Assets/Scripts/LaucherScript.cs
+++ Assets/Scripts/LaucherScript.cs
@@ -9,6 +9,9 @@ public class LaucherScript : MonoBehaviour {
     public LayerMask whatIsExplodable;
     public float rocketSpeed;
 
+    private Camera cam;
+    private bool inWall = false;
+
     public GameObject testhit;
 
     private readonly float delay = 0.8f;
@@ -18,20 +21,56 @@ public class LaucherScript : MonoBehaviour {
 
     private void Start() {
         recoil = GetComponent<Animation>();
+        cam = Camera.main;
     }
+
     void Update() {
         time += Time.deltaTime;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit testhit, 1000f, whatIsExplodable)) {
+            //Vector3 direction = (testhit.point - spawnPos.transform.position).normalized; //get direction from center of screen to laucher position
+            //Debug.Log(Vector3.Distance(testhit.point, spawnPos.position));
+
+        }
+
+
         if (Input.GetButton("Fire1") && time > delay) {
             recoil.Play("LauncherRecoil");
-            GameObject tempRocket = Instantiate(rocket, spawnPos.position, transform.rotation);
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 1000f, whatIsExplodable)) {
-                Vector3 direction = (hit.point - spawnPos.transform.position).normalized; //get direction from raycast to laucher
-                tempRocket.GetComponent<Rigidbody>().AddForce(direction * rocketSpeed, ForceMode.Impulse);
+
+
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, 1000f, whatIsExplodable)) {
+                Debug.Log(Vector3.Distance(testhit.point, spawnPos.position));
+                if (Vector3.Distance(testhit.point, spawnPos.position) < 1.4f) { //if in wall
+                    GameObject tempRocket = Instantiate(rocket, hit.point, transform.rotation);
+                    tempRocket.GetComponent<Rigidbody>().AddForce(transform.forward * .01f , ForceMode.Impulse); //delte this mayb 
+                    //testhit.transform.position = hit.point;
+
+                }
+                else {
+                    GameObject tempRocket = Instantiate(rocket, spawnPos.position, transform.rotation);
+                    Vector3 direction = (hit.point - spawnPos.transform.position).normalized; //get direction from center of screen to laucher position
+                    tempRocket.GetComponent<Rigidbody>().AddForce(direction * rocketSpeed, ForceMode.Impulse);
+                }
             }
-            else
+            else {
+                GameObject tempRocket = Instantiate(rocket, spawnPos.position, transform.rotation);
                 tempRocket.GetComponent<Rigidbody>().AddForce(transform.forward * rocketSpeed, ForceMode.Impulse);
+            }
             time = 0;
         }
     }
-}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Environment")) {
+            inWall = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Environment")) {
+            Debug.Log("exiting wall");
+            inWall = false;
+        }
+    }
+}   
 
